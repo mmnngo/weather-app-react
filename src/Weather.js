63f6b5b72d7bcf7ba66c00e676ae2284886d08d7
mmnngo <94153115+mmnngo@users.noreply.github.com>
2022-08-07 18:11:41 -0400

    Forecast integration, added WeatherIcon

--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,17 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       date: new Date(response.data.dt * 1000),
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       maxTemp: response.data.main.temp_max,
       minTemp: response.data.main.temp_min,
@@ -22,18 +24,22 @@ export default function Weather(props) {
       iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   function search() {
     const apiKey = "65b5662e22ff1bb3952c072792011a99";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
+
   if (weatherData.ready) {
     return (
       <div className="card">
@@ -60,6 +66,8 @@ export default function Weather(props) {
             </div>
           </form>
           <WeatherInfo data={weatherData} />
+          <hr />
+          <WeatherForecast coordinates={weatherData.coordinates} />
         </div>
       </div>
     );
---
+++ src/WeatherForecast.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "b5662e22ff1bb3952c072792011a99";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
